8568817a6055cc546cd351f99deb5a41499aefcc
Gizmo675 <septier.romain@gmail.com>
2020-01-29 09:25:47 +0100

    creation du footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from '../Navbar'
+import Navbar from '../Navbar';
+import Footer from '../Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <h2>Je suis le footer</h2>
+    </div>
+  );
+}
+
+export default Footer;
