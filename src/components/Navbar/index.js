13ccfd52ce3149a690279fb0090c380d6316b05f
Gizmo675 <septier.romain@gmail.com>
2020-01-29 09:18:30 +0100

    creation du menu

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <li>
+        Acceuil
+      </li>
+      <li>
+        portfolio
+      </li>
+      <li>
+        Blog
+      </li>
+      <li>
+        Contact
+      </li>
+    </div>
+  );
+}
+
+export default Navbar;
